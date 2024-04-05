270a8d49185ca3352129ede15f46d04cacdb8125
paul <paul@MAINPC>
2024-04-05 15:57:34 +0300

    initial

---
+++ fw.ino
@@ -0,0 +1,121 @@
+#include <util/atomic.h>
+
+// Pins
+#define ENCA 2
+#define ENCB 3
+#define PWM 9
+
+// globals
+long prevT = 0;
+int posPrev = 0;
+// Use the "volatile" directive for variables
+// used in an interrupt
+volatile int pos_i = 0;
+volatile float velocity_i = 0;
+volatile long prevT_i = 0;
+
+float v1Filt = 0;
+float v1Prev = 0;
+float v2Filt = 0;
+float v2Prev = 0;
+
+float eintegral = 0;
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(ENCA,INPUT);
+  pinMode(ENCB,INPUT);
+  pinMode(PWM,OUTPUT);
+
+  attachInterrupt(digitalPinToInterrupt(ENCA),
+                  readEncoder,RISING);
+}
+
+void loop() {
+    static float last_vt = 0;
+
+  // read the position in an atomic block
+  // to avoid potential misreads
+  int pos = 0;
+  float velocity2 = 0;
+  ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
+    pos = pos_i;
+    velocity2 = velocity_i;
+  }
+
+  // Compute velocity with method 1
+  long currT = micros();
+  float deltaT = ((float) (currT-prevT))/1.0e6;
+  float velocity1 = (pos - posPrev)/deltaT;
+  posPrev = pos;
+  prevT = currT;
+
+  // Convert count/s to encoder pulse freq
+  float v1 = velocity1/4.0;
+  float v2 = velocity2/4.0;
+
+  // Low-pass filter (25 Hz cutoff)
+  v1Filt = 0.854*v1Filt + 0.0728*v1 + 0.0728*v1Prev;
+  v1Prev = v1;
+  v2Filt = 0.854*v2Filt + 0.0728*v2 + 0.0728*v2Prev;
+  v2Prev = v2;
+
+  // Set a target
+  float vt = analogRead(A0) / 1024.0 * (750.0 - 25.0) + 25.0;
+  if (fabs(vt - last_vt) > 50)
+  {
+    eintegral = 0;
+    last_vt = vt;
+  }
+
+  // Compute the control signal u
+  float kp = 1.5;
+  float ki = 2;
+  float e = vt - fabs(v1Filt);
+  eintegral = eintegral + e*deltaT;
+  
+  float u = kp*e + ki*eintegral;
+
+  // Set the motor speed and direction
+  int pwr = (int)u;
+  if(pwr > 255 || isinff(u)){
+    pwr = 255;
+  }
+  else if (pwr < 0)
+  {
+    pwr = 0;
+  }
+  setMotor(1,pwr,PWM);
+
+  Serial.print(vt);
+  Serial.print(" ");
+  Serial.print(v1Filt);
+  Serial.println();
+  delay(1);
+}
+
+void setMotor(int dir, int pwmVal, int pwm){
+  analogWrite(pwm,pwmVal); // Motor speed
+}
+
+void readEncoder(){
+  // Read encoder B when ENCA rises
+  int b = digitalRead(ENCB);
+  int increment = 0;
+  if(b>0){
+    // If B is high, increment forward
+    increment = 1;
+  }
+  else{
+    // Otherwise, increment backward
+    increment = -1;
+  }
+  pos_i = pos_i + increment;
+
+  // Compute velocity with method 2
+  long currT = micros();
+  float deltaT = ((float) (currT - prevT_i))/1.0e6;
+  velocity_i = increment/deltaT;
+  prevT_i = currT;
+}
